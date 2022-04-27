6e8e4b8af335d77c8f8ac47e53abdf237acc2b9c
DragonDev <thanhlongedu0304@gmail.com>
2022-04-27 21:01:18 +0700

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <!-- Navigator -->
+  <NavigatorComponent />
+  <RouterView />
 </template>
 
+<script>
+import NavigatorComponent from "./components/navigator.vue";
+
+export default {
+  components: {
+    NavigatorComponent,
+  },
+  setup() {},
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url("./assets/scss/index.scss");
 </style>
