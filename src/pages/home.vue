783ef6ba45e49159804507e9af1c71591d744079
HoangHa2808 <hadoan12a1@gmail.com>
2022-05-01 17:54:21 +0700

    Ha them bootstrap do roi nha

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,123 @@
 <template>
-  <h1>Hello</h1>
+  <!-- <h1>Hello</h1> -->
+  <main class="home">
+    <div class="container">
+      <div id="myCarousel" class="carousel slide">
+        <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+          <div class="col-md-6 px-0">
+            <h1 class="display-4 fst-italic">Title of</h1>
+            <p class="lead my-3">
+              "Multiple lines of text that form the lede, informing new readers
+              quickly and efficiently about what’s most interesting in this
+              post’s contents."
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- End Carousel -->
+
+      <div class="row row--space-between">
+        <div class="col-xs-12 col-md-4">
+          <div class="post_thumb">
+            <a href="">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                src="" alt="" />
+              </svg>
+            </a>
+          </div>
+          <div class="post_text">
+            <a class="post_catpost" href="">Travel</a>
+          </div>
+          <h3 class="post_title typescale-1">
+            <a href=""
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </a>
+          </h3>
+        </div>
+
+        <div class="col-xs-12 col-md-4">
+          <div class="post_thumb">
+            <a href="">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                src="" alt="" />
+              </svg>
+            </a>
+          </div>
+          <div class="post_text">
+            <a class="post_catpost" href="">Travel</a>
+          </div>
+          <h3 class="post_title typescale-1">
+            <a href=""
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </a>
+          </h3>
+        </div>
+
+        <div class="col-xs-12 col-md-4">
+          <div class="post_thumb">
+            <a href="">
+              <svg
+                class="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                role="img"
+                aria-label="Placeholder: 140x140"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect>
+                src="" alt="" />
+              </svg>
+            </a>
+          </div>
+          <div class="post_text">
+            <a class="post_catpost" href="">Travel</a>
+          </div>
+          <h3 class="post_title typescale-1">
+            <a href=""
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </a>
+          </h3>
+        </div>
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End container -->
+  </main>
 </template>
 
 <script>
 export default {
-  name: "HomePage",
+  name: "HomePages",
 };
 </script>
 
-<style></style>
+<style lang="scss" latest news>
+// @use "../assets/scss/variables.scss" as *;
+.home {
+  max-width: 1200px;
+  height: 1000px;
+  margin: 0 auto;
+}
+</style>
