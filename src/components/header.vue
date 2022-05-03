42e9f813a75080565da4d2c33cd5ee9ac1b5c968
NgocAnhsne <ngocanhne2402@gmail.com>
2022-05-03 23:11:47 +0700

    them nhanh

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header-page">
+
+  </header>
+</template>
+
+<script>
+import NavigatorComponent form '../components/navigator.vue';
+export default {
+  name: "HeaderComponent",
+}
+</script>
+
+<style>
+
+</style>
