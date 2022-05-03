8a7e26f5b07ff9a836159e1d38c79d6f5562ffc9
DragonDev <thanhlongedu0304@gmail.com>
2022-05-03 22:13:55 +0700

    new init

--- src/components/last_news_item.vue
+++ src/components/last_news_item.vue
@@ -2,16 +2,31 @@
   <div class="last-news-item">
     <!-- news image -->
     <div class="news-image">
-      <img src="" alt="news image" />
+      <img
+        src="https://images.unsplash.com/photo-1651237130541-f6af6bfdaa3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="news image"
+      />
     </div>
-    <!-- news category -->
-    <p class="news-category">Business</p>
-    <!-- news title -->
+    <div class="news-info">
+      <!-- news category -->
+      <p class="news-category">Business</p>
+      <!-- news title -->
+      <h4 class="news-title">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat dolor
+        natus dolorum molestiae quas illo architecto? Blanditiis
+      </h4>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "LastNewsItem",
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.last-news-item {
+  display: flex;
+}
+</style>
